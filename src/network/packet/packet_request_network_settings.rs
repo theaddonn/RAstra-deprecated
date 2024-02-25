9404e7c7a85b9cd28b077912c5569309872ad337
Adrian <adrian.olbrich1@gmail.com>
2024-02-25 15:50:59 +0100

    added de-batching support

---
+++ src/network/packet/packet_request_network_settings.rs
@@ -0,0 +1,23 @@
+use crate::error::RastraError;
+use crate::error::RastraError::CouldNotDeserialize;
+use crate::network::gamepacket::GamepacketClient;
+use crate::network::packet_io::packet_io_reader::PacketReader;
+
+pub struct PacketRequestNetworkSettings {
+    pub protocol_version: u32,
+}
+
+impl GamepacketClient for PacketRequestNetworkSettings {
+    fn deserialize(data: &[u8]) -> Result<Self, RastraError> {
+        let mut reader = PacketReader::new_game_packet_reader(Vec::from(data));
+
+        reader.read_u64_varint().unwrap();
+
+        let protocol_version = match reader.read_u32() {
+            Ok(val) => val,
+            Err(_) => return Err(CouldNotDeserialize),
+        };
+
+        Ok(Self { protocol_version })
+    }
+}
