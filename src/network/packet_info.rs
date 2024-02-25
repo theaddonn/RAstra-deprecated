cd9637d5429db31b1cfc13a2c7594c457d77f316
Adrian <adrian.olbrich1@gmail.com>
2024-02-25 15:52:55 +0100

    added magic

--- src/network/packet_info.rs
+++ src/network/packet_info.rs
@@ -4,6 +4,10 @@ pub const RAKNET_GAME_PACKET_ID: u8 = 0xfe;
 pub const PROTOCOL_VERSION: u32 = 630;
 pub const MINECRAFT_VERSION: &'static str = "1.20.50";
 
+pub const MAGIC: [u8; 16] = [
+    0x00, 0xff, 0xff, 0x0, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78,
+];
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum GamePacket {
     Login = 1,
