f60355304ffbe16c0f2401130cb6f9d597cdcb5e
adrian <adrian.olbrich1@gmail.com>
2024-04-25 21:24:19 +0200

    update data

--- bedrock-rs/protocol/src/info.rs
+++ bedrock-rs/protocol/src/info.rs
@@ -1,6 +1,6 @@
 pub const RAKNET_GAME_PACKET_ID: u8 = 0xfe;
-pub const PROTOCOL_VERSION: u32 = 630;
-pub const MINECRAFT_VERSION: &'static str = "1.20.50";
+pub const PROTOCOL_VERSION: u32 = 671;
+pub const MINECRAFT_VERSION: &'static str = "1.20.80";
 
 pub const MAGIC: [u8; 16] = [
     0x00, 0xff, 0xff, 0x0, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78,
