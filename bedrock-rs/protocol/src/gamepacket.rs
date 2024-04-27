f0985f8af408eab031710ca95b9ac644e89bfbbb
Adrian <adrian.olbrich1@gmail.com>
2024-04-27 17:59:01 +0200

    rust fmt

--- bedrock-rs/protocol/src/gamepacket.rs
+++ bedrock-rs/protocol/src/gamepacket.rs
@@ -1,7 +1,7 @@
 use crate::info::GamePacketType;
 
 pub fn encode_gamepacket(data: &Vec<u8>, game_packet_type: GamePacketType) -> Vec<u8> {
-vec![]
+    vec![]
 }
 
 pub fn decode_gamepacket(data: &Vec<u8>) -> (Vec<u8>, GamePacketType) {
