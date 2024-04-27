87f7ed003080b8341c43cddbcc6b7ea0495747b4
Adrian <adrian.olbrich1@gmail.com>
2024-04-27 13:21:23 +0200

    rust fmt

--- bedrock-rs/bedrock_core/src/types.rs
+++ bedrock-rs/bedrock_core/src/types.rs
@@ -10,59 +10,59 @@ use std::fmt::{Debug, Display, Formatter};
 
 // i16
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i16le (pub i16);
+pub struct i16le(pub i16);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i16be (pub i16);
+pub struct i16be(pub i16);
 
 // u16
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u16le (pub u16);
+pub struct u16le(pub u16);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u16be (pub u16);
+pub struct u16be(pub u16);
 
 // i32
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i32le (pub i32);
+pub struct i32le(pub i32);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i32be (pub i32);
+pub struct i32be(pub i32);
 
 // u32
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u32le (pub u32);
+pub struct u32le(pub u32);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u32be (pub u32);
+pub struct u32be(pub u32);
 
 // i64
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i64le (pub i64);
+pub struct i64le(pub i64);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i64be (pub i64);
+pub struct i64be(pub i64);
 
 // u64
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u64le (pub u64);
+pub struct u64le(pub u64);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u64be (pub u64);
+pub struct u64be(pub u64);
 
 // i128
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i128le (pub i128);
+pub struct i128le(pub i128);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct i128be (pub i128);
+pub struct i128be(pub i128);
 
 // u128
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u128le (pub u128);
+pub struct u128le(pub u128);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct u128be (pub u128);
+pub struct u128be(pub u128);
 
 // f32
 // use normal f32
@@ -72,7 +72,7 @@ pub struct u128be (pub u128);
 
 // uvarint 32
 #[derive(Debug, Copy, Clone)]
-pub struct uvar32 (pub u32);
+pub struct uvar32(pub u32);
 
 // ivarint 32
 #[derive(Debug, Copy, Clone)]
@@ -84,7 +84,7 @@ pub struct uvar64(pub u64);
 
 // ivarint 64
 #[derive(Debug, Copy, Clone)]
-pub struct ivar64 (pub i64);
+pub struct ivar64(pub i64);
 
 // bool
 // use normal bool
