f2d9c2733bc9b0b44a9f4f63b6cbaa841db16f6c
Adrian <adrian.olbrich1@gmail.com>
2024-04-27 13:21:07 +0200

    added more tests

--- bedrock-rs_test/src/main.rs
+++ bedrock-rs_test/src/main.rs
@@ -1,35 +1,96 @@
-use std::io::{Bytes, Read};
-use std::net::{Ipv4Addr, SocketAddrV4};
-use tokio::main;
-use tokio::net::UdpSocket;
-use raknet_rs::server::{ConfigBuilder, MakeIncoming};
+use std::io::Cursor;
 
-use futures_util::StreamExt;
-use futures_util::SinkExt;
+use bedrock_rs::core::de::MCDeserialize;
+use bedrock_rs::core::ser::MCSerialize;
+use bedrock_rs::core::types::u16le;
+use bedrock_rs::protocol::info::GamePacketType;
+use bedrock_rs::protocol::packets::login::LoginPacket;
+use bedrock_rs::protocol::packets::network_settings::NetworkSettingsPacket;
+use bedrock_rs::protocol::packets::network_settings_request::NetworkSettingsRequestPacket;
+use bytes::{Buf, BufMut};
+use rak_rs::mcpe::motd::Gamemode;
+use rak_rs::Motd;
+use tokio::main;
+use varint_rs;
+use varint_rs::{VarintReader, VarintWriter};
 
 #[main]
 async fn main() {
-    let socket = UdpSocket::bind(SocketAddrV4::new(
-        Ipv4Addr::new(127, 0, 0, 1),
-        19132
-    )).await.unwrap();
+    let mut listener = rak_rs::server::Listener::bind("127.0.0.1:19132").await.unwrap();
+
+    listener.motd = Motd {
+        edition: String::from("MCPE"),
+        name: String::from("Bedrock Test"),
+        sub_name: String::from("Rastra"),
+        protocol: bedrock_rs::protocol::info::PROTOCOL_VERSION as u16,
+        version: String::from("1.20.80"),
+        player_count: 1,
+        player_max: 10,
+        gamemode: Gamemode::Survival,
+        server_guid: rand::random(),
+        port: Some(String::from("19132")),
+        ipv6_port: Some(String::from("19133")),
+        nintendo_limited: Some(false),
+    };
+
+    listener.versions = &[9, 10, 11];
+
+    listener.id = rand::random();
+
+    listener.start().await.unwrap();
 
 
-    let config = ConfigBuilder::default()
-        .min_mtu(128)
-        .max_mtu(1024)
-        .send_buf_cap(1024*2*2*2*2)
-        .sever_guid(123456789)
-        .advertisement(bytes::Bytes::copy_from_slice(b"HELLO"))
-        .support_version(vec![9, 10, 11])
-        .max_pending(128)
+    println!("started!");
 
-        .build()
-        .unwrap();
+    let mut conn = listener.accept().await.unwrap();
 
-    let mut incoming = socket.make_incoming(config);
+    let mut data = Cursor::new(conn.recv().await.unwrap());
 
-    let mut io = incoming.next().await.unwrap();
-    let data: bytes::Bytes = io.next().await.unwrap();
-    io.send(data).await.unwrap();
+    data.get_u8();
+    data.read_u64_varint().unwrap();
+    data.read_u64_varint().unwrap();
+
+    let pk = NetworkSettingsRequestPacket::deserialize(
+        &mut data
+    ).unwrap();
+
+    println!("{:#?}", pk);
+
+    let nspk = NetworkSettingsPacket {
+        compression_threshold: u16le(0),
+        compression_algorythm: u16le(1),
+        client_throttle_enabled: false,
+        client_throttle_threshold: 0,
+        client_throttle_scalar: 0.0,
+    };
+
+    let mut nspk_buf = vec![];
+
+    nspk_buf.write_u64_varint(GamePacketType::NetworkSettings as u64).unwrap();
+    nspk_buf.put_slice(&*nspk.serialize().unwrap());
+
+    let mut buf = vec![];
+    buf.put_u8(bedrock_rs::protocol::info::RAKNET_GAME_PACKET_ID);
+    buf.put_u8(0);
+    buf.write_u64_varint(nspk_buf.len() as u64).unwrap();
+
+    buf.put_slice(&*nspk_buf);
+
+    conn.send(&*buf, false).await.unwrap();
+
+    println!("{:#?}", nspk);
+
+    let mut data = Cursor::new(conn.recv().await.unwrap());
+
+    data.get_u8();
+    data.get_u8();
+    data.read_u64_varint().unwrap();
+    data.read_u64_varint().unwrap();
+
+    let pk = LoginPacket::deserialize(
+        &mut data
+    ).unwrap();
+
+    println!("{:#?}", pk);
 }
+
