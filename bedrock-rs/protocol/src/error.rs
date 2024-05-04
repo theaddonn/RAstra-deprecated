4c5d97cee7f55470143ece8bbcd37a8d00fac930
Adrian <adrian.olbrich1@gmail.com>
2024-05-04 12:52:34 +0200

    remove unneeded ProtocolError options

--- bedrock-rs/protocol/src/error.rs
+++ bedrock-rs/protocol/src/error.rs
@@ -1,10 +1,9 @@
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ProtocolError {
-    ReadPacketBufError,
-    ReadUtf8StringError,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ListenerError {
     AddrBindErr,
 }
+
