6c4d2de180287114e7e37484f1657cac6e3e3e9d
Adrian <adrian.olbrich1@gmail.com>
2024-05-04 19:36:53 +0200

    add encryption and compression as modules

--- bedrock-rs/protocol/src/lib.rs
+++ bedrock-rs/protocol/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod batch;
-pub mod client;
+pub mod conn;
 pub mod error;
 pub mod gamepacket;
 pub mod info;
@@ -7,3 +7,5 @@ pub mod listener;
 pub mod login;
 pub mod packets;
 pub mod types;
+pub mod compression;
+pub mod encryption;
