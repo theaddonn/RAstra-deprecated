bc312d395a140bfb744dbcf415b504ff9b143985
Adrian <adrian.olbrich1@gmail.com>
2024-05-05 15:25:58 +0200

    add more errors

--- bedrock-rs/protocol/src/error.rs
+++ bedrock-rs/protocol/src/error.rs
@@ -1,20 +1,37 @@
+use serialize::error::{DeserilizationError, SerilizationError};
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum ProtocolError {
-}
+pub enum ProtocolError {}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ListenerError {
-    AddrBindErr,
+    AddrBindError,
+    AlreadyOnline,
+    NotListening,
+}
+
+#[derive(Debug)]
+pub enum ConnectionError {
+    ReadIOError,
+    WriteIOError,
+    SerializeError(SerilizationError),
+    DeserializeError(DeserilizationError),
+    ConnectionClosed,
+    RaknetError,
+    CompressError(CompressionError),
+    DecompressError(DecompressionError),
+    InvalidRaknetHeader,
+    InvalidCompressionMethod,
 }
 
 #[derive(Debug)]
 pub enum CompressionError {
     ZlibError(flate2::CompressError),
-    SnappyError(snap::Error)
+    SnappyError(snap::Error),
 }
 
 #[derive(Debug)]
 pub enum DecompressionError {
     ZlibError(flate2::DecompressError),
-    SnappyError(snap::Error)
+    SnappyError(snap::Error),
 }
