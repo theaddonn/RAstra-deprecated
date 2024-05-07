e833233432e565031a9a85d710299431de0ef7c5
Adrian <adrian.olbrich1@gmail.com>
2024-05-07 18:51:58 +0200

    fix problems

--- bedrock-rs/protocol/src/login.rs
+++ bedrock-rs/protocol/src/login.rs
@@ -1,94 +1,111 @@
 use bedrock_core::types::u16le;
-use crate::compression::CompressionMethod;
+
+use crate::compression::none::NoCompression;
+use crate::compression::snappy::SnappyCompression;
+use crate::compression::zlib::ZlibCompression;
+use crate::compression::{CompressionMethod, CompressionMethods};
 use crate::conn::Connection;
-use crate::error::{ConnectionError, LoginError};
+use crate::error::LoginError;
 use crate::gamepacket::GamePacket;
-use crate::info::PROTOCOL_VERSION;
 use crate::packets::network_settings::NetworkSettingsPacket;
 
 pub struct LoginServerSideOptions {
-    pub Compression: Box<dyn CompressionMethod>,
-    pub Encryption: bool,
-    // TODO: impl Microsoft Auth
-    pub AuthenticationEnabled: bool,
-    /// Even if the client has another protocol version
-    pub AllowOtherProtocols: bool,
+    pub compression: CompressionMethods,
+    pub encryption: bool,
+    /// TODO: impl Microsoft Auth
+    pub authentication_enabled: bool,
+    /// Even if the client has another protocol version the login
+    /// process should continue, might cause problems when the login process
+    /// differs in the different protocol versions
+    pub allowed_proto_versions: Vec<i32>,
 }
 
-pub async fn handle_login_server_side(connection: &mut Connection, options: LoginServerSideOptions) -> Result<(), LoginError>{
+pub async fn handle_login_server_side(
+    connection: &mut Connection,
+    options: LoginServerSideOptions,
+) -> Result<(), LoginError> {
     // Recieve NetworkRequestSettings
     let gamepackets = match connection.recv_gamepackets().await {
-        Ok(v) => { v }
-        Err(_) => { return Err(LoginError::PacketMissmatch) }
+        Ok(v) => v,
+        Err(_) => return Err(LoginError::PacketMissmatch),
     };
 
-    // If too many or no packets were send the error
+    // If too many or no packets were send then error
     if gamepackets.len() > 1 || gamepackets.len() < 1 {
-        return Err(LoginError::PacketMissmatch)
+        return Err(LoginError::PacketMissmatch);
     }
 
     // Get the clients protocol version
     let client_proto_ver = match gamepackets[0] {
-        GamePacket::RequestNetworkSettings(pk) => {
-            pk.client_network_version.0
-        }
-        _ => {
-            return Err(LoginError::PacketMissmatch)
-        }
+        GamePacket::RequestNetworkSettings(pk) => pk.client_network_version.0,
+        _ => return Err(LoginError::PacketMissmatch),
     };
 
     // Look if Protocol versions match and if other protocol versions are allowed
-    if !options.AllowOtherProtocols {
-        if client_proto_ver != PROTOCOL_VERSION {
-            return Err(LoginError::WrongProtocolVersion {
-                server: PROTOCOL_VERSION,
-                client: client_proto_ver,
-            })
-        }
+    if !options.allowed_proto_versions.contains(&client_proto_ver) {
+        return Err(LoginError::WrongProtocolVersion {
+            client: client_proto_ver,
+        });
+    }
+
+    println!("NETWORK SETTINGS REQUEST");
+
+    // Get the compression threshold and id for the network settings packet
+    let (threshold, id_u16) = match &options.compression {
+        CompressionMethods::Zlib(zlib) => (zlib.threshold, ZlibCompression::ID_u16),
+        CompressionMethods::Snappy(snappy) => (snappy.threshold, SnappyCompression::ID_u16),
+        CompressionMethods::None => (0, NoCompression::ID_u16),
     };
 
     // Send Network Settings
-    match connection.send_gamepackets(vec![GamePacket::NetworkSettings(
-        NetworkSettingsPacket {
-            compression_threshold: u16le(options.Compression.get_threshold()),
-            compression_algorythm: u16le(options.Compression.get_IDu16()),
+    match connection
+        .send_gamepackets(vec![GamePacket::NetworkSettings(NetworkSettingsPacket {
+            compression_threshold: u16le(threshold),
+            compression_algorythm: u16le(id_u16),
             // TODO: Figure out what all of this is
             client_throttle_enabled: false,
             client_throttle_threshold: 0,
             client_throttle_scalar: 0.0,
-        }
-    )]).await {
+        })])
+        .await
+    {
         Ok(_) => {}
-        Err(e) => { return Err(LoginError::ConnError(e)) }
+        Err(e) => return Err(LoginError::ConnError(e)),
     };
 
-    connection.set_compression_method(Some(
-       options.Compression
-    ));
+    println!("NETWORK SETTINGS");
+
+    connection.set_compression_method(Some(options.compression));
 
     // Recieve Login
     let gamepackets = match connection.recv_gamepackets().await {
-        Ok(v) => { v }
-        Err(_) => { return Err(LoginError::PacketMissmatch) }
+        Ok(v) => v,
+        Err(e) => {
+            println!("{:?}", e);
+            return Err(LoginError::PacketMissmatch);
+        }
     };
+
+    println!("LOGING #1");
 
     // If too many or no packets were send the error
     if gamepackets.len() > 1 || gamepackets.len() < 1 {
-        return Err(LoginError::PacketMissmatch)
+        return Err(LoginError::PacketMissmatch);
     }
+
+    println!("LOGING #2");
 
     // Get the clients protocol version
     let login_pk = match &gamepackets[0] {
-        GamePacket::Login(pk) => {
-            pk
-        }
-        _ => {
-            return Err(LoginError::PacketMissmatch)
-        }
+        GamePacket::Login(pk) => pk,
+        _ => return Err(LoginError::PacketMissmatch),
     };
 
-    Ok(())
+    println!("LOGING #3 {:#?}", login_pk);
 
+    println!("LOGIN");
+
+    Ok(())
 }
 
 pub fn handle_login_client_side(connection: &mut Connection) {
